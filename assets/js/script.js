7ba07db54d800fc17c9e0536bc84829345a3ff40
Emily Salinas <emilyrh1058@gmail.com>
2021-03-20 13:54:54 -0500

    add drag/drop features to js and html

--- assets/js/script.js
+++ assets/js/script.js
@@ -228,3 +228,20 @@ $(".card .list-group").sortable({
   }
   });
 
+  // make the tasks dropable for
+  $("#trash").droppable({
+    accept: ".card .list-group-item",
+    tolerance: "touch",
+    drop: function(event, ui) {
+      ui.draggable.remove();
+      console.log("drop");
+    },
+    over: function(event, ui) {
+      console.log("over");
+    },
+    out: function(event, ui) {
+      console.log("out");
+    }
+  });
+
+
